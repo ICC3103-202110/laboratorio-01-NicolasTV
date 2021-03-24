d4c1587c08f7f405cc135d47c828378e27fabae5
Nicolás Torres <natorres@miuandes.cl>
2021-03-24 18:26:39 -0300

    1st commit

---
+++ Nicolas_Torres_Lab01.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 13:52:49 2021
+
+@author: nator
+"""
+import random 
+
+
+username_1=input("Hola! Ingresa el nombre de usuario 1: ")
+username_2=input("Nombre de usuario 2: ")
+
+n_cards=int(input("¿Con cuántas cartas van a jugar? "))
+
+while n_cards % 2 != 0:
+    print("Debes ingresar un número par de cartas")
+    n_cards=int(input("¿Con cuántas cartas van a jugar? "))
+
+
+
+
+    
+
+     
+    
+    
+    
