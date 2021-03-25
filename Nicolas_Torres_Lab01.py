be884d92d86137a683108e0a608bef5517009e99
Nicolás Torres <natorres@miuandes.cl>
2021-03-24 21:55:45 -0300

    2nd commit

--- Nicolas_Torres_Lab01.py
+++ Nicolas_Torres_Lab01.py
@@ -5,23 +5,31 @@ Created on Wed Mar 24 13:52:49 2021
 @author: nator
 """
 import random 
+import numpy as np
 
 
-username_1=input("Hola! Ingresa el nombre de usuario 1: ")
-username_2=input("Nombre de usuario 2: ")
+user_1=input("Hola! Ingresa el nombre de usuario 1: ")
+user_2=input("Nombre de usuario 2: ")
 
 n_cards=int(input("¿Con cuántas cartas van a jugar? "))
-
 while n_cards % 2 != 0:
     print("Debes ingresar un número par de cartas")
     n_cards=int(input("¿Con cuántas cartas van a jugar? "))
 
+row = int(n_cards/2) 
+column = int(n_cards/2)
 
+matrix_1 = []
+matrix_2 = []
 
-
-    
-
-     
-    
-    
+for i in range(row):
+    a=i+1
+    matrix_1.append([a])
+    for j in range(column):
+        b=j+1
+        matrix_1.append([b])
+        
+print(matrix_1)
+       
+   
     
