f312132d84066f8289b7442e454f5840fe8d0279
Nicolás Torres <natorres@miuandes.cl>
2021-03-25 19:16:40 -0300

    3rd commit

--- Nicolas_Torres_Lab01.py
+++ Nicolas_Torres_Lab01.py
@@ -5,8 +5,6 @@ Created on Wed Mar 24 13:52:49 2021
 @author: nator
 """
 import random 
-import numpy as np
-
 
 user_1=input("Hola! Ingresa el nombre de usuario 1: ")
 user_2=input("Nombre de usuario 2: ")
@@ -19,17 +17,21 @@ while n_cards % 2 != 0:
 row = int(n_cards/2) 
 column = int(n_cards/2)
 
-matrix_1 = []
-matrix_2 = []
 
-for i in range(row):
-    a=i+1
-    matrix_1.append([a])
-    for j in range(column):
-        b=j+1
-        matrix_1.append([b])
-        
-print(matrix_1)
-       
+def Front_cards():
+    numbers=[]
+    a=1
+    for i in range(row): 
+        numbers.append([a])
+        a+=1  
+    return numbers*2
+
+for r in range(row):
+    for c in range(column):
+        print(Front_cards[r][c],end=" ")
+      
+
+    
+
    
     
