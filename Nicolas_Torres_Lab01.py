7cc488bf6ffdc8bb3fea6c83ef8ea7cd6663739b
Nicolás Torres <natorres@miuandes.cl>
2021-03-26 11:01:41 -0300

    create matrix

--- Nicolas_Torres_Lab01.py
+++ Nicolas_Torres_Lab01.py
@@ -4,34 +4,43 @@ Created on Wed Mar 24 13:52:49 2021
 
 @author: nator
 """
-import random 
+import random
 
-user_1=input("Hola! Ingresa el nombre de usuario 1: ")
-user_2=input("Nombre de usuario 2: ")
+def generate_cards(n):
+  numbers = []
+  for i in range(1, n + 1):
+    numbers += [i, i]
+  
+  return numbers
 
-n_cards=int(input("¿Con cuántas cartas van a jugar? "))
-while n_cards % 2 != 0:
-    print("Debes ingresar un número par de cartas")
-    n_cards=int(input("¿Con cuántas cartas van a jugar? "))
+def build_game(array):
+  front_matrix = []
+  back_matrix = []
 
-row = int(n_cards/2) 
-column = int(n_cards/2)
+  for i in range(0, len(array), 2):
+    front_matrix.append(['*', '*'])
+    back_matrix.append([array[i], array[i + 1]])
+
+  return front_matrix, back_matrix
+    
 
 
-def Front_cards():
-    numbers=[]
-    a=1
-    for i in range(row): 
-        numbers.append([a])
-        a+=1  
-    return numbers*2
-
-for r in range(row):
-    for c in range(column):
-        print(Front_cards[r][c],end=" ")
+        
       
 
     
+print('--------- Bienvenido al Memorize! ---------\n')
+user_1 = input("Ingresa el nombre de usuario 1: ")
+user_2 = input("Nombre de usuario 2: ")
+n_pairs = int(input("\n¿Con cuántos pares de cartas van a jugar? "))
+
+turn = 0
+points = [0, 0]
+players = [user_1, user_2]
+
+while n_pairs % 2 != 0 and n_pairs > 0:
+    print("Debes ingresar un número par de cartas")
+    n_pairs = int(input("¿Con cuántos pares de cartas a jugar? "))
 
    
     
